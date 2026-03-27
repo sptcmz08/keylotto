50f7bb9f41e611c63376210444486c5ef6da25be
Werachai Nachiangmai <ae1nt14@gmail.com>
2026-03-27 23:39:46 +0700

    fix: add auth to debug script

--- admin/debug_winlose.php
+++ admin/debug_winlose.php
@@ -1,6 +1,7 @@
 <?php
-require_once 'config.php';
-header('Content-Type: text/html; charset=utf-8');
+require_once __DIR__ . '/../auth.php';
+requireLogin();
+require_once __DIR__ . '/../config.php';
 
 $lotteryId = intval($_GET['lottery'] ?? 0);
 $date = $_GET['date'] ?? date('Y-m-d');
